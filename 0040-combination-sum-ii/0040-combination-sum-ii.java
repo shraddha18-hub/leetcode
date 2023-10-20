1a1b2e8dafabe2015d05228cff242cde1425297c
shraddha18-hub <91208834+shraddha18-hub@users.noreply.github.com>
2023-10-20 16:41:00 +0530

    Time: 2 ms (99.88%), Space: 42.1 MB (75.77%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,31 +1,29 @@
 class Solution {
-    List<List<Integer>> result=new ArrayList<>();
-    private void findcombinations(int ind, int[] arr, int target, List<Integer>ds)
+    
+    private void findcombinations(int ind, int [] arr, int target, List<List<Integer>>ans,
+                                 List<Integer>ds)
     {
-         if(target == 0)
-          {
-            result.add(new ArrayList<>(ds));
-          }
-          
-        else if(target>0)
+        if(target == 0)
         {
-             for(int i=ind;i<arr.length;i++){
-                if (i > ind && arr[i] == arr[i - 1]) {
-                    continue;
-                }
-            ds.add(arr[i]);
-            findcombinations(i+1,arr,target - arr[i], ds);
-            ds.remove(ds.size()-1);
+            ans.add(new ArrayList<>(ds));
+            return;
         }
-         //findcombinations(ind+1,arr,target, ans, ds);
-        
-    }
+        for(int i=ind; i<arr.length; i++)
+        {
+            if(i > ind && arr[i]==arr[i-1])
+             continue;
+            if(arr[i]>target) break;
+            
+        ds.add(arr[i]);
+        findcombinations(i+1,arr,target-arr[i],ans,ds);
+        ds.remove(ds.size()-1);
+        }
+       
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
         List<List<Integer>>ans = new ArrayList<>();
-        findcombinations(0, candidates, target, new ArrayList<>());
-        return result;
-        
+        Arrays.sort(candidates);
+        findcombinations(0,candidates,target,ans,new ArrayList<>());
+        return ans;
     }
 }
